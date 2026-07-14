31ccbf5441816686e6722d983c7131f12094ef32
Apolinapolis <larra12@ya.ru>
2026-07-14 19:18:40 +0300

    api_client refactor

--- tests/clients/api_client.py
+++ tests/clients/api_client.py
@@ -1,9 +1,9 @@
 import requests
-
 from tests.settings import BASE_URL
 
 
 class APIClient:
+    # Base methods
     def post(self, path, **kwargs):
         return requests.post(f"{BASE_URL}{path}", timeout=5, **kwargs)
 
@@ -15,6 +15,33 @@ class APIClient:
 
     def delete(self, path, **kwargs):
         return requests.delete(f"{BASE_URL}{path}", timeout=5, **kwargs)
+
+    # Auth logic
+    def register_user(self, username, password):
+        return self.post("/register", json={"username": username, "password": password})
+
+    def login_user(self, username, password):
+        return self.post("/login", data={"username": username, "password": password})
+
+    @staticmethod
+    def get_auth_headers(token:str)->dict:
+        return {"Authorization": f"Bearer {token}"}
+
+    # Habits logic
+    def create_habit(self, token:str, payload:dict):
+        return self.post("/habits", json=payload, headers=self.get_auth_headers(token))
+
+    def get_habits(self, token:str):
+        return self.get("/habits", headers=self.get_auth_headers(token))
+
+    def get_habit_by_id(self, token:str, habit_id:str):
+        return self.get(f"/habits/{habit_id}", headers=self.get_auth_headers(token))
+
+    def delete_habit(self, token:str, habit_id:str):
+        return self.delete(f"/habits/{habit_id}", headers=self.get_auth_headers(token))
+
+    def update_habit(self, token:str, habit_id:str, payload:dict):
+        return self.patch(f"/habits/{habit_id}", headers=self.get_auth_headers(token), json=payload)
 
 
 api_client = APIClient()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,38 +1,61 @@
 import pytest
-
+import uuid
 from app.models.db_models import HabitDB, UserDB
 from app.schemas.habit import HabitCreate, HabitUpdate
+from tests.clients.api_client import api_client
+from tests.settings import DEFAULT_PASSWORD
 
 
+#UNIT
 @pytest.fixture
 def user() -> UserDB:
     return UserDB(id=1, hashed_password="hoo", username="tester")
 
-
 @pytest.fixture
 def existing_habit_factory():
     def create(habit_id=2, owner_id=1, title="exist_habit_title", description="exist_habit_description"):
-        return HabitDB(
-            title=title,
-            description=description,
-            id=habit_id,
-            owner_id=owner_id,
-        )
-
+        return HabitDB(title=title, description=description,id=habit_id,owner_id=owner_id,)
     return create
-
 
 @pytest.fixture
 def habit_create_factory():
     def create(title="test_habit_create_title", description="test_habit_create_description"):
         return HabitCreate(title=title, description=description)
-
     return create
-
 
 @pytest.fixture
 def update_habit_factory():
     def create(title=None, description=None):
         return HabitUpdate(title=title, description=description)
-
     return create
+
+# API
+@pytest.fixture
+def user_payload_factory():
+    def create(username=None,password=DEFAULT_PASSWORD):
+        if username is None:
+            username = f"user_{uuid.uuid4().hex}"
+        return {"username": username,"password": password}
+    return create
+
+
+@pytest.fixture
+def habit_payload_factory():
+    def create(title:str = "smoke tree", description:str = "enjoy every moment"):
+        return {"title": title, "description": description}
+    return create
+
+
+@pytest.fixture
+def token(user_payload_factory):
+    credentials = user_payload_factory()
+    api_client.register_user(**credentials)
+    response = api_client.login_user(**credentials)
+    return response.json()["access_token"]
+
+
+@pytest.fixture
+def created_habit(token, habit_payload_factory):
+    payload = habit_payload_factory()
+    response = api_client.create_habit(token, payload)
+    return response.json()
